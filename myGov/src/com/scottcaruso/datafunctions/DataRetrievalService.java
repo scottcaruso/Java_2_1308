0f81501b1897f1ce867ffb82d60dc4956d0ccbbd
Scott Caruso <scottcaruso@fullsail.edu>
2013-08-06 23:05:40 -0400

    Service is working. Incorrect entries handled. NOTE: There is a crash when searching for a second zip code. Memory leak?

--- myGov/src/com/scottcaruso/datafunctions/DataRetrievalService.java
+++ myGov/src/com/scottcaruso/datafunctions/DataRetrievalService.java
@@ -5,12 +5,21 @@ import org.json.JSONObject;
 import com.scottcaruso.interfacefunctions.DisplayPoliticianResults;
 import com.scottcaruso.mygov.MainActivity;
 
+import android.app.Activity;
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
 import android.widget.Toast;
 
 public class DataRetrievalService extends IntentService{
 
+	public static final String MESSENGER_KEY = "messenger";
+	public static final String ZIP_KEY = "URL";
+	
 	public DataRetrievalService() 
 	{
 		super("DataRetrievalService");
@@ -20,17 +29,24 @@ public class DataRetrievalService extends IntentService{
 	@Override
 	protected void onHandleIntent(Intent intent) 
 	{		
-		String dataResponse = RetrieveDataFromSunlightLabs.retrieveData(urlString);
+		Log.i("onHandleIntent","Service started");
+		
+		Bundle extras = intent.getExtras();
+		Messenger messenger = (Messenger) extras.get(MESSENGER_KEY);
+		String zipString = (String) extras.get(ZIP_KEY);
+		String dataResponse = RetrieveDataFromSunlightLabs.retrieveData("http://congress.api.sunlightfoundation.com/legislators/locate?zip="+zipString+"&apikey=eab4e1dfef1e467b8a25ed1eab0f7544");
 		JSONObject parsedObject = TurnStringIntoJSONObject.createMasterObject(dataResponse);
+
 		//Handle if the user has entered an unknown/invalid zip code.
-		if (parsedObject == null)
+		Message message = Message.obtain();
+		message.arg1 = Activity.RESULT_OK;
+		message.obj = parsedObject;
+		
+		try 
 		{
-			Toast toast = Toast.makeText(MainActivity.getCurrentContext(), "You have entered an invalid zip code. Please try again.", Toast.LENGTH_LONG);
-			toast.show();
-		} else
-		{
-			DisplayPoliticianResults.showPoliticiansInMainView(parsedObject, false);
+			messenger.send(message);
+		} catch (RemoteException e) {
+			Log.e("messenger","There was a problem sending the message.");
 		}
 	}
-
 }
--- myGov/src/com/scottcaruso/mygov/MainActivity.java
+++ myGov/src/com/scottcaruso/mygov/MainActivity.java
@@ -6,14 +6,20 @@ package com.scottcaruso.mygov;
 
 import org.json.JSONObject;
 
+import com.scottcaruso.datafunctions.DataRetrievalService;
 import com.scottcaruso.datafunctions.RetrieveDataFromSunlightLabs;
 import com.scottcaruso.datafunctions.SaveFavoritesLocally;
 import com.scottcaruso.interfacefunctions.DisplayPoliticianResults;
 import com.scottcaruso.utilities.Connection_Verification;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +29,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 	
 	static Context currentContext;
+	public static JSONObject jsonResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +53,44 @@ public class MainActivity extends Activity {
 				if (connected)
 				{
 					String enteredZip = zipEntry.getText().toString();
-					RetrieveDataFromSunlightLabs.retrieveData("http://congress.api.sunlightfoundation.com/legislators/locate?zip="+enteredZip+"&apikey=eab4e1dfef1e467b8a25ed1eab0f7544");
+					//RetrieveDataFromSunlightLabs.retrieveData("http://congress.api.sunlightfoundation.com/legislators/locate?zip="+enteredZip+"&apikey=eab4e1dfef1e467b8a25ed1eab0f7544");
+					Handler retrievalHandler = new Handler()
+					{
+
+						@Override
+						public void handleMessage(Message msg) 
+						{
+							if (msg.arg1 == RESULT_OK)
+							{
+								try {
+									jsonResponse = (JSONObject) msg.obj;
+								} catch (Exception e) {
+									Log.e("Error","There was a problem retrieving the json Response.");
+								}
+								if (jsonResponse == null)
+								{
+									Toast toast = Toast.makeText(MainActivity.getCurrentContext(), "You have entered an invalid zip code. Please try again.", Toast.LENGTH_LONG);
+									toast.show();
+								} else
+								{
+									DisplayPoliticianResults.showPoliticiansInMainView(jsonResponse, false);
+								}
+							}
+						}
+						
+					};
+					
+					Messenger apiMessenger = new Messenger(retrievalHandler);
+					
+					Intent startDataService = new Intent(MainActivity.this, DataRetrievalService.class);
+					startDataService.putExtra(DataRetrievalService.MESSENGER_KEY, apiMessenger);
+					startDataService.putExtra(DataRetrievalService.ZIP_KEY,enteredZip);
+					startService(startDataService);
+					
+					//Toast toast = Toast.makeText(MainActivity.this, "Retrieving data...", Toast.LENGTH_SHORT);
+					//toast.show();
+
+				
 				} else
 				{
 					Toast toast = Toast.makeText(MainActivity.this, "There is no connection to the internet available. Please try again later, or view saved politicians.", Toast.LENGTH_LONG);
